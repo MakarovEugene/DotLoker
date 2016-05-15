0e36b113b84acc7be6af82c57c0407f5d3b5e4e5
MakarovEugene <mjenya9@gmail.com>
2016-05-15 16:43:27 +0300

    DotLoker_Base versio

--- DotLoker/GameScene.swift
+++ DotLoker/GameScene.swift
@@ -7,39 +7,101 @@
 //
 
 import SpriteKit
-
+import Darwin
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
-    }
+    var Dot=SKSpriteNode()
+    /*let Xlabel=SKLabelNode()
+    let YLabel=SKLabelNode()*/
+    var dotDied:Bool=true
+    var dotArray=[SKSpriteNode] ()
+    var serealCount: Int=0
+    /*var serialDot:Bool=false*/
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+    
+    override func didMoveToView(view: SKView) //Первональная загрузка игровой сцены
+    {
+        self.backgroundColor=UIColor.blackColor()
+        if dotArray.isEmpty==true
+        {
+            dotSerial(3)
+            /*AddDot()*/
+            dotDied=false
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) //Обработка нажатия кнопки
+    {
+        for touch in touches
+        {
+            let touchPoint=touch.locationInNode(self)
+            if dotArray.isEmpty==false
+            {
+                for index in 0...dotArray.count-1
+                {
+                    if dotArray[index].containsPoint(touchPoint)
+                    {
+                        dotArray[index].removeFromParent()
+                        dotArray.removeAtIndex(index)
+                        break
+                    }
+                    else
+                    {
+                        continue
+                    }
+                }
+            }
+            else
+            {
+                dotDied=true
+                print ("The status of Array Empty is \(dotArray.isEmpty)")
+            }
+        }
+    }
+    
+    func dotSerial (serialCount:Int)//Функция создания нескольких точек
+    {
+            if dotArray.isEmpty==true
+            {
+                for _ in 1...serialCount
+                {
+                    AddDot()
+                    dotArray.append(Dot)
+                    dotDied=false
+                }
+            }
+        
+    }
+    func AddDot() //Функция генерации точки в рандомном месте с ограничением по Y=maxY-100
+    {
+        
+        let randX=CGFloat(arc4random_uniform(UInt32(CGRectGetMaxX(self.frame))))
+        let randY=CGFloat(arc4random_uniform(UInt32(CGRectGetMaxY(self.frame)-100)))
+        Dot=SKSpriteNode(imageNamed: "Dot")
+        Dot.position=CGPoint(x: randX, y: randY)
+        Dot.size=CGSize(width: 45, height: 45)
+        self.addChild(Dot)
+        /*Xlabel.text=String(randX)
+        YLabel.text=String(randY)
+        Xlabel.fontColor=UIColor.blackColor()
+        YLabel.fontColor=UIColor.blackColor()
+        Xlabel.fontSize=60
+        YLabel.fontSize=60
+        Xlabel.position=CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        YLabel.position=CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame)-Xlabel.fontSize-10)
+        addChild(Xlabel)
+        addChild(YLabel)*/
+ 
+ 
+    }
+    override func update(currentTime: CFTimeInterval) //Функция обновления точки на экране
+    {
         /* Called before each frame is rendered */
+        if dotArray.isEmpty==true
+        {
+         if dotDied==true
+         {
+            dotSerial(3)
+            dotDied=false
+         }
+        }
     }
 }
